cb418c05b25a4f822cea5659dea26fff1ed41613
Gessicabarth <geh.barth@gmail.com>
2026-04-28 19:10:57 -0300

    Estudando e entendendo funções

---
+++ funcoes/arrowFunctions.js
@@ -0,0 +1,16 @@
+function somaDoisNumeros(a, b) {
+    return a + b;
+}
+
+let resultado = somaDoisNumeros(8,7);
+
+
+
+// com arrow function
+const somaDoisNumeros = (a, b) => a + b;
+
+
+let resultado = somaDoisNumeros(8,7);
+
+
+
---
+++ funcoes/funcoesConstrutoras.js
@@ -0,0 +1,9 @@
+function Animal(nome, especie) {
+    this.nome = nome;
+    this.especie = especie;
+}
+
+const animal1 = new Animal("Megui", "cachorro")
+
+console.log(animal1.nome);
+ 
